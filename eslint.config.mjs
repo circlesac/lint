bdb08e5400c0ce0a9d1e9f81287d5fe0f45df374
choidabom <48302257+choidabom@users.noreply.github.com>
2025-09-09 15:44:30 +0900

    feat: add common build folders to ESLint ignore list

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,7 +5,7 @@ export default tseslint.config(
 	eslint.configs.recommended,
 	...tseslint.configs.recommended,
 	{
-		ignores: ["dist", "coverage"]
+		ignores: ["dist", "coverage", "build", "node_modules", "**/.sst/**", "**/.open-next/**", "**/.next/**"]
 	},
 	{
 		languageOptions: {
